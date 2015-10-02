20dcce40179f47c203bf4cc8d7007ba0100a9b17
junerey <junerey.casuga@gmail.com>
2015-10-02 18:52:44 +0800

    fix on searching from history

--- resources/views/app/search.blade.php
+++ resources/views/app/search.blade.php
@@ -1,5 +1,4 @@
 @extends('app')
-
 <div class="container-fluid">
     <div class="row map-container">
         <div id="map" style="width: 100%; height: 95%"></div>
@@ -7,7 +6,7 @@
     <div class="row">
         <input type="hidden" id="_token" name="_token" value="{{ csrf_token() }}">
         <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
-            <input type="text" id="city-input" class="form-control" placeholder="Enter a City" value={{$q}}>
+            <input type="text" id="city-input" class="form-control" placeholder="Enter a City" value="{{ $q }}">
         </div>
         <input type="submit" class="btn btn-primary col-xs-3 col-sm-3 col-md-3 col-lg-3" value="Search" id="searchBtn">
         <a href="/history" class="btn btn-primary col-xs-3 col-sm-3 col-md-3 col-lg-3">History</a>
